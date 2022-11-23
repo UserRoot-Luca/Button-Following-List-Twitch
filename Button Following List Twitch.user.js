d332b258cc57a5697835a02332decdbc2bd7a6fb
UserRoot-Luca <55756898+UserRoot-Luca@users.noreply.github.com>
2022-11-23 18:29:28 +0100

    V 1.2

--- Button Following List Twitch.user.js
+++ Button Following List Twitch.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Button Following List Twitch
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  ###
 // @author       UserRoot-Luca
 // @match        https://www.twitch.tv/*
@@ -18,14 +18,15 @@
                 let e_list = e.target.children;
                 let e_last = e_list[e_list.length - 1];
                 let e_userName = e_last.querySelector(".chat-author__display-name");
-                if (e_userName) {
+                let e_className = e_last.querySelector(".DBFollowing");
+                if (e_userName && !e_className) {
                     let BadgeContainer = e_last.querySelector(".chat-line__username-container > span");
                     let MyBadge = document.createElement("div");
                     MyBadge.setAttribute("class", "InjectLayout-sc-1i43xsx-0 hDgGYp");
                     let userName = e_userName.textContent;
                     if (userName) {
                         let urlUserName = `https://www.twitchdatabase.com/following/${userName}`;
-                        MyBadge.innerHTML = `<button data-a-target="chat-badge" onclick="window.open('${urlUserName}', '_blank');"> 
+                        MyBadge.innerHTML = `<button class="DBFollowing" data-a-target="chat-badge" onclick="window.open('${urlUserName}', '_blank');"> 
                             <img alt="DB" class="chat-badge" src="https://www.twitchdatabase.com/favicon.ico" style="width: 18px; height: 18px; border-radius: 3px;">
                         </button>`;
                     }
